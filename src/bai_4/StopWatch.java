0bb2f2c469bdb4bf45305998eb09dcbc60e8f8d4
võ văn tín <vovantin9502@gmail.com>
2023-03-09 23:31:53 +0700

    first commit

--- src/bai_4/StopWatch.java
+++ src/bai_4/StopWatch.java
@@ -3,91 +3,30 @@ package bai_4;
 import java.util.Scanner;
 
 public class StopWatch {
-//    long startTime;
-//    long endTime;
-//    String start;
-//    String end;
-//
-//
-//    public long startTime() {
-//        if (start == "s") ;
-//
-//
-//        return System.currentTimeMillis();
-//
-//
-//    }
-//
-//    public long endTime() {
-//        if (end == "e") ;
-//
-//        return System.currentTimeMillis();
-//
-//    }
-//
-//    public long getElapsedTime() {
-//        return (endTime() - startTime());
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        System.out.println("nhap s de bat dau: ");
-//        Scanner b = new Scanner(System.in);
-//        String start = b.nextLine();
-//        System.out.println("nhap e de ket thuc: ");
-//        String end = b.nextLine();
-//
-//        StopWatch a = new StopWatch(start,end);
-//        System.out.println(a.getElapsedTime());
-//    }
-//package class_and_object_java.bai_tap;
-//
-//import java.util.Scanner;
-//
-//    public class StopWatch {
-        long starttime;
-        long endTime;
-
-
-        public StopWatch(long starttime, long endTime) {
-            this.starttime = starttime;
-            this.endTime = endTime;
-        }
-
-        public long getStarttime() {
-            return starttime;
-        }
-
-        public long getEndTime() {
-            return endTime;
-        }
-
-        public static long start() {
-            long a1 = System.currentTimeMillis();
-            return a1;
-        }
-
-        public static long stop() {
-            long b1 = System.currentTimeMillis();
-            return b1;
-        }
-
-        public static long getElapsedTime(long a, long b) {
-            long c = b - a;
-            return c;
-        }
-
-        public static void main(String[] args) {
-            long a = start();
-            System.out.println(a);
-            Scanner input = new Scanner(System.in);
-            System.out.println("Nhập vào 1 để ngừng thời gian ");
-            int click = input.nextInt();
-            if (click == 1) {
-                long b = stop();
-                System.out.println(b);
-                System.out.println("Thời gian  đã trôi qua là :" + getElapsedTime(a, b) + " milisSecond");
-            }
-        }
+    public static long start() {
+        long k = System.currentTimeMillis();
+        return k;
     }
 
+    public static long stop() {
+        long h = System.currentTimeMillis();
+        return h;
+    }
+
+    public static long getElapsedTime(long m, long n) {
+        long c = n - m;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        long m = start();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap 0 de tinh thoi gian: ");
+        int end = sc.nextInt();
+        if (end == 0) {
+            long n = stop();
+            System.out.println(n);
+            System.out.println("Thoi gian can tinh  :" + getElapsedTime(m, n) + " milisSecond");
+        }
+    }
+}
