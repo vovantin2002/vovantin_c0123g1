febe53fd2c65b7c6a2d14892388cf2b3af36fa7b
võ văn tín <vovantin9502@gmail.com>
2023-03-22 14:27:28 +0700

    first commit

--- src/bai_14/CaiDatThuatToanSapXepChen.java
+++ src/bai_14/CaiDatThuatToanSapXepChen.java
@@ -1,4 +1,23 @@
 package bai_14;
 
+import java.util.Arrays;
+
 public class CaiDatThuatToanSapXepChen {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 6, 78, 9, 2};
+        int x;
+        int pos;
+        for (int i = 1; i < arr.length; i++) {
+            x = arr[i];
+            pos = i;
+            while (0 < pos && x < arr[pos - 1]) {
+                arr[pos] = arr[pos - 1];
+                pos--;
+                System.out.println("Lần thu " + i + "- " + pos + " " + Arrays.toString(arr));
+            }
+            arr[pos] = x;
+
+            System.out.println("Sau lan thu " + i + ": " + Arrays.toString(arr));
+        }
+    }
 }
--- src/bai_14/MinhHoaThuatToanSapXepChen.java
+++ src/bai_14/MinhHoaThuatToanSapXepChen.java
@@ -1,5 +1,24 @@
 package bai_14;
 
-public class MinhHoaThuatToanSapXepChen {
+import java.util.Arrays;
 
+public class MinhHoaThuatToanSapXepChen {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 6, 78, 9, 2};
+        for (int i = 0; i < arr.length; i++) {
+            int indexMin = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[indexMin]) {
+                    indexMin = j;
+                }
+            }
+            if (indexMin != i) {
+                int temp = arr[i];
+                arr[i] = arr[indexMin];
+                arr[indexMin] = temp;
+                System.out.println("Lan thu " + i + ": " + Arrays.toString(arr));
+            }
+        }
+    }
 }
+
